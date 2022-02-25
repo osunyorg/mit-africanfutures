adf4e7f8eb3e818de31a87ed71d154d49c022ec5
Arnaud Levy <contact@arnaudlevy.com>
2022-02-25 09:39:48 +0100

    theme

--- postcss.config.js
+++ postcss.config.js
@@ -7,30 +7,30 @@ module.exports = {
                 'layouts/**/*.html'
             ],
             safelist: {
-              standard: [
-                'show',
-                'active',
-                'collapsed',
-                'modal-backdrop',
-                /^dropdown/,
-                /^nav-level-/,
-                /^is-/,
-                /^has-/,
-                /^js-/
-              ],
-              greedy: [
-                /administrators__/,
-                /articles__/,
-                /authors__/,
-                /categories__/,
-                /page__/,
-                /pages__/,
-                /persons__/,
-                /posts__/,
-                /programs__/,
-                /teachers__/,
-                /volumes__/
-              ]
+                standard: [
+                    'show',
+                    'active',
+                    'collapsed',
+                    'modal-backdrop',
+                    /^dropdown/,
+                    /^nav-level-/,
+                    /^is-/,
+                    /^has-/,
+                    /^js-/
+                ],
+                greedy: [
+                    /administrators__/,
+                    /articles__/,
+                    /authors__/,
+                    /categories__/,
+                    /page__/,
+                    /pages__/,
+                    /persons__/,
+                    /posts__/,
+                    /programs__/,
+                    /teachers__/,
+                    /volumes__/
+                ]
             }
         },
         autoprefixer: {},

b00f597a2e1333138966a2e455c3bdf8e5b9746b
alexisben <alexiben7@gmail.com>
2022-07-06 21:57:29 +0200

    upgrade

--- postcss.config.js
+++ postcss.config.js
@@ -1,43 +1,75 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-    plugins: {
+/* eslint-disable no-undef */
+var devPlugins = {},
+    productionPlugins = {
+        autoprefixer: {},
+        cssnano: {
+            preset: 'default'
+        },
         '@fullhuman/postcss-purgecss': {
             content: [
+                // './hugo_stats.json',
                 './themes/**/*.html',
                 'layouts/**/*.html'
             ],
+            // defaultExtractor: (content) => {
+            //     let els = JSON.parse(content).htmlElements;
+            //     return els.tags.concat(els.classes, els.ids);
+            // },
             safelist: {
                 standard: [
                     'show',
                     'active',
                     'collapsed',
-                    'modal-backdrop',
-                    'stretched-link',
                     /^dropdown/,
-                    /^ratio/,
                     /^nav-level-/,
+                    /^splide_/,
                     /^is-/,
                     /^has-/,
-                    /^js-/
+                    /^js-/,
+
+                    // Glightbox
+                    'wait-autoplay',
+                    'gfadeIn',
+                    'gfadeOut',
+                    'gslideOutLeft',
+                    'gslideInLeft',
+                    'gslideOutRight',
+                    'gslideInRight',
+                    'gzoomIn',
+                    'gzoomOut'
+                ],
+                deep: [
+                    // Glightbox
+                    /^glightbox/,
+                    /^gslide/,
+                    /^desc-top/,
+                    /^desc-left/,
+                    /^ginlined/,
+                    /^zoomed/,
+                    /^gdesc-/,
+                    /^gabsolute/,
+                    /^grelative/,
+                    /^gloader/,
+                    /^goverlay/,
+                    /^gprev/,
+                    /^gnext/,
+                    /^gclose/,
+                    /^gbtn/,
+                    /^gcontainer/
                 ],
                 greedy: [
-                    /administrators__/,
-                    /articles__/,
-                    /authors__/,
-                    /categories__/,
-                    /page__/,
-                    /pages__/,
-                    /persons__/,
-                    /posts__/,
-                    /programs__/,
-                    /teachers__/,
-                    /volumes__/
+                    /__home/,
+                    /__page/,
+                    /__section/,
+                    /__term/,
+                    /posts/,
+                    /call_to_action/,
+                    /block-timeline/
                 ]
             }
-        },
-        autoprefixer: {},
-        cssnano: {
-            preset: 'default'
         }
-    }
+    };
+
+module.exports = {
+    plugins: process.env.HUGO_ENVIRONMENT === 'production' ? productionPlugins : devPlugins
 };

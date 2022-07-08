5b912274673169bba7498e2424914dcc7de4e4c8
alexisben <alexiben7@gmail.com>
2022-07-08 09:30:38 +0200

    postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,75 +1,79 @@
 /* eslint-disable no-undef */
-var devPlugins = {},
-    productionPlugins = {
-        autoprefixer: {},
-        cssnano: {
-            preset: 'default'
-        },
-        '@fullhuman/postcss-purgecss': {
-            content: [
-                // './hugo_stats.json',
-                './themes/**/*.html',
-                'layouts/**/*.html'
-            ],
-            // defaultExtractor: (content) => {
-            //     let els = JSON.parse(content).htmlElements;
-            //     return els.tags.concat(els.classes, els.ids);
-            // },
-            safelist: {
-                standard: [
-                    'show',
-                    'active',
-                    'collapsed',
-                    /^dropdown/,
-                    /^nav-level-/,
-                    /^splide_/,
-                    /^is-/,
-                    /^has-/,
-                    /^js-/,
-
-                    // Glightbox
-                    'wait-autoplay',
-                    'gfadeIn',
-                    'gfadeOut',
-                    'gslideOutLeft',
-                    'gslideInLeft',
-                    'gslideOutRight',
-                    'gslideInRight',
-                    'gzoomIn',
-                    'gzoomOut'
-                ],
-                deep: [
-                    // Glightbox
-                    /^glightbox/,
-                    /^gslide/,
-                    /^desc-top/,
-                    /^desc-left/,
-                    /^ginlined/,
-                    /^zoomed/,
-                    /^gdesc-/,
-                    /^gabsolute/,
-                    /^grelative/,
-                    /^gloader/,
-                    /^goverlay/,
-                    /^gprev/,
-                    /^gnext/,
-                    /^gclose/,
-                    /^gbtn/,
-                    /^gcontainer/
-                ],
-                greedy: [
-                    /__home/,
-                    /__page/,
-                    /__section/,
-                    /__term/,
-                    /posts/,
-                    /call_to_action/,
-                    /block-timeline/
-                ]
-            }
-        }
-    };
-
 module.exports = {
-    plugins: process.env.HUGO_ENVIRONMENT === 'production' ? productionPlugins : devPlugins
+    autoprefixer: {},
+    cssnano: {
+        preset: 'default'
+    },
+    '@fullhuman/postcss-purgecss': {
+        content: [
+            // './hugo_stats.json',
+            './themes/**/*.html',
+            'layouts/**/*.html'
+        ],
+        // defaultExtractor: (content) => {
+        //     let els = JSON.parse(content).htmlElements;
+        //     return els.tags.concat(els.classes, els.ids);
+        // },
+        safelist: {
+            standard: [
+                'show',
+                'active',
+                'collapsed',
+                'h1',
+                'h2',
+                'h3',
+                'h4',
+                'h5',
+                'h6',
+                /^dropdown/,
+                /^nav-level-/,
+                /^splide_/,
+                /^is-/,
+                /^has-/,
+                /^js-/,
+
+                // Glightbox
+                'wait-autoplay',
+                'gfadeIn',
+                'gfadeOut',
+                'gslideOutLeft',
+                'gslideInLeft',
+                'gslideOutRight',
+                'gslideInRight',
+                'gzoomIn',
+                'gzoomOut'
+            ],
+            deep: [
+                // Glightbox
+                /block-/,
+                /ratio/,
+                /^glightbox/,
+                /^gslide/,
+                /^desc-top/,
+                /^desc-left/,
+                /^ginlined/,
+                /^zoomed/,
+                /^gdesc-/,
+                /^gabsolute/,
+                /^grelative/,
+                /^gloader/,
+                /^goverlay/,
+                /^gprev/,
+                /^gnext/,
+                /^gclose/,
+                /^gbtn/,
+                /^gcontainer/
+            ],
+            greedy: [
+                /__home/,
+                /__page/,
+                /__section/,
+                /__term/,
+                /posts/,
+                /call_to_action/,
+                /block-timeline/,
+                /__taxonomy/
+            ]
+        }
+    }
 };
